3c4b80d9bd045c0787f3740acc5c132c540bae3f
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-25 12:53:39 +0000

    Automatically backed up by Learn

--- app/controllers/dog.rb
+++ app/controllers/dog.rb
@@ -2,10 +2,17 @@ class Dog
   attr_accessor :name, :age
   attr_reader :breed
   
+  @all = []
+  
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+    self.save
+  end
+  
+  def save
+    @all << self
   end
   
 end
